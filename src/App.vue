ba5c47ca601b0aa189a3c3567cc7bc2278efd36d
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2021-06-08 21:45:16 -0300

    Alura 4

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
-<div>
-  <h1 v-text="titulo"></h1>
-  <ul>
-    <li v-for="foto of fotos" :key="foto.titulo">
-      <img :src="foto.url" :alt="foto.titulo">
-    </li>    
-  </ul>
-</div>
+  <div class="corpo">
+    <h1 class="centralizado" v-text="titulo"></h1>
+    
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos" :key="foto.titulo">        
+        <meu-painel :titulo="foto.titulo">          
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+      </li>    
+    </ul>
+  </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
+  components: {
+    'meu-painel': Painel
+  },
   data() {
     return {
       titulo: 'Alura Pic',
@@ -26,5 +34,25 @@ export default {
 </script>
 
 <style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;    
+  }
 
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }  
 </style>
